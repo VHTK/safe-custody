e9b623a48bc1f0a77f273946ddca7e7b6dd28360
zhangtao <zhangtao2@onchain.com>
2022-12-13 15:13:32 +0800

    modify lod

--- custody-backend/src/main/java/com/bourne/controller/TestController.java
+++ custody-backend/src/main/java/com/bourne/controller/TestController.java
@@ -3,6 +3,7 @@ package com.bourne.controller;
 import com.bourne.feign.ProductFeignService;
 import com.bourne.result.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,18 @@ public class TestController {
     @Autowired
     private ProductFeignService productFeignService;
 
+    @Value("${user.name}")
+    private String testStr;
+
     @GetMapping("/get")
     public Integer testGet(@RequestParam Long id){
         Result result = productFeignService.detail(id);
         return 1;
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return testStr;
     }
 }
 
