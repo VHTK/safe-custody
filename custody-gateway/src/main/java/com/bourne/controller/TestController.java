520708248a1ffc9c561d2a2f5bb98824cdc8faa6
maojian_ohhey <maojian_ohhey@163.com>
2022-12-14 15:51:28 +0800

    nacos connect test

--- custody-gateway/src/main/java/com/bourne/controller/TestController.java
+++ custody-gateway/src/main/java/com/bourne/controller/TestController.java
@@ -1,17 +1,24 @@
 package com.bourne.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
 
-    @Value("${user.name}")
-    private String testStr;
+    @Value("${user.desc}")
+    @Getter
+    @Setter
+    private String value;
 
     @GetMapping("/test")
-    private String test(){
-        return testStr;
+    private String test() {
+        return getValue();
+
     }
 }
