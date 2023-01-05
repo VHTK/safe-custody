08a510ed25703600f4108310b53df821e1a77c91
zhangtao <zhangtao2@onchain.com>
2023-01-05 16:31:28 +0800

    transfer ERC20

--- custody-chainbase/src/main/java/com/bourne/Service/WalletService.java
+++ custody-chainbase/src/main/java/com/bourne/Service/WalletService.java
@@ -5,15 +5,12 @@ import org.web3j.abi.FunctionEncoder;
 import org.web3j.abi.FunctionReturnDecoder;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
-import org.web3j.abi.datatypes.Bool;
 import org.web3j.abi.datatypes.Function;
 import org.web3j.abi.datatypes.Type;
 import org.web3j.abi.datatypes.generated.Uint256;
-import org.web3j.crypto.CipherException;
-import org.web3j.crypto.Credentials;
-import org.web3j.crypto.WalletUtils;
+import org.web3j.abi.datatypes.generated.Uint8;
+import org.web3j.crypto.*;
 import org.web3j.protocol.Web3j;
-import org.web3j.protocol.admin.methods.response.PersonalUnlockAccount;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.DefaultBlockParameterNumber;
@@ -21,8 +18,8 @@ import org.web3j.protocol.core.methods.request.Transaction;
 import org.web3j.protocol.core.methods.response.*;
 import org.web3j.protocol.websocket.WebSocketClient;
 import org.web3j.protocol.websocket.WebSocketService;
-import org.web3j.tx.Transfer;
 import org.web3j.utils.Convert;
+import org.web3j.utils.Numeric;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,15 +32,20 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 public class WalletService {
 
     private static WebSocketService webSocketService;
     private static String walletFilePath;
+    private static String emptyAddress;
 
     static {
+         emptyAddress = "0x0000000000000000000000000000000000000000";
         walletFilePath = "/Users/zhangtao/Documents/walletfile";
         try {
             WebSocketClient webSocketClient = new WebSocketClient(new URI("wss://goerli.infura.io/ws/v3/b497e3a7cb6649128a25aa1653fbb70e"));
@@ -71,8 +73,9 @@ public class WalletService {
         // ETH
         System.out.println(getBalance(address));
 
-        String hash = transfer(walletName,password,"0x30635bA975d8e9116b79d8CCd8c6250C05328f3d",BigDecimal.valueOf(0.01D));
-        System.out.println(hash);
+        System.out.println(transfer(walletName,password,"0x30635bA975d8e9116b79d8CCd8c6250C05328f3d",BigDecimal.valueOf(0.001)));
+
+        System.out.println(transferToken(walletName,password,"0x30635bA975d8e9116b79d8CCd8c6250C05328f3d",contractAddress,BigDecimal.valueOf(50)));
     }
 
     public  static String createWallet(String walletName, String password) throws InvalidAlgorithmParameterException, CipherException, IOException, NoSuchAlgorithmException, NoSuchProviderException {
@@ -125,67 +128,173 @@ public class WalletService {
     }
 
     /**
-     * 发送ETH
+     * ETH 交易
      * @param walletName
      * @param password
      * @param toAddress
-     * @param amount
-     * @throws Exception
+     * @param value
+     * @return
+     * @throws CipherException
+     * @throws IOException
+     * @throws ExecutionException
+     * @throws InterruptedException
      */
-    public static String transfer(String walletName, String password, String toAddress, BigDecimal amount) throws Exception {
+    public static String transfer(String walletName, String password,String toAddress, BigDecimal value) throws CipherException, IOException, ExecutionException, InterruptedException {
+        Web3j web3j = Web3j.build(webSocketService);
+
+        //加载转账所需的凭证，用私钥
         String walleFilePath = walletFilePath + File.separator + walletName;
         Credentials credentials = WalletUtils.loadCredentials(password, walleFilePath);
-        Web3j web3j = Web3j.build(webSocketService);
-        TransactionReceipt send = Transfer.sendFunds(web3j, credentials, toAddress, amount, Convert.Unit.ETHER).send();
-        log.info("Transaction complete:");
-        log.info("trans hash=" + send.getTransactionHash());
-        log.info("from :" + send.getFrom());
-        log.info("to:" + send.getTo());
-        log.info("gas used=" + send.getGasUsed());
-        log.info("status: " + send.getStatus());
-        return send.getTransactionHash();
+        String fromAddress = getWalletAddress(walletName, password);
+            //获取nonce，交易笔数
+            BigInteger nonce = getNonce(web3j, fromAddress);
+
+        // TODO
+        //gasPrice和gasLimit 都可以手动设置
+        BigInteger gasPrice = getGasPrice(web3j);
+        if (gasPrice == null) {
+            log.error("GetGasPriceError from:[{}] to:[{}] amount:[{}]", fromAddress, toAddress, value.toString());
+            return null;
+        }
+        //BigInteger.valueOf(4300000L) 如果交易失败 很可能是手续费的设置问题
+        BigInteger gasLimit = BigInteger.valueOf(60000L);
+
+            //创建RawTransaction交易对象
+            RawTransaction rawTransaction = RawTransaction.createEtherTransaction(nonce, gasPrice, gasLimit, toAddress, Convert.toWei(value, Convert.Unit.ETHER).toBigIntegerExact());
+            //签名Transaction，这里要对交易做签名
+            byte[] signMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+            String hexValue = Numeric.toHexString(signMessage);
+            //发送交易
+            EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(hexValue).sendAsync().get();
+            return ethSendTransaction.getTransactionHash();
     }
 
     /**
-     * 发送ETH
+     * ERC20 交易
      * @param walletName
      * @param password
      * @param toAddress
-     * @param amount
-     * @throws Exception
+     * @param contractAddress
+     * @param value
+     * @return
      */
-    public static String transferToken(String walletName, String password, String toAddress, BigInteger amount, String contractAddress) throws Exception {
+    public static String transferToken(String walletName, String password, String toAddress, String contractAddress, BigDecimal value) throws CipherException, IOException, ExecutionException, InterruptedException {
         Web3j web3j = Web3j.build(webSocketService);
 
-        String methodName = "transfer";
+       value =  value.multiply(BigDecimal.TEN.pow(getTokenDecimals(web3j,contractAddress)));
+
+        //加载转账所需的凭证，用私钥
+        String walleFilePath = walletFilePath + File.separator + walletName;
+        Credentials credentials = WalletUtils.loadCredentials(password, walleFilePath);
+        String fromAddress = getWalletAddress(walletName, password);
+        //获取nonce，交易笔数
+        BigInteger nonce = getNonce(web3j, fromAddress);
+        if (nonce == null) {
+            log.error("getNonceError from:[{}] to:[{}] amount:[{}] contract [{}]", fromAddress, toAddress, value, contractAddress);
+            return null;
+        }
+        // TODO
+        //gasPrice和gasLimit 都可以手动设置
+        BigInteger gasPrice = getGasPrice(web3j);
+        if (gasPrice == null) {
+            log.error("getGasPriceError from:[{}] to:[{}] amount:[{}] contract [{}]", fromAddress, toAddress, value, contractAddress);
+            return null;
+        }
+        //BigInteger.valueOf(4300000L) 如果交易失败 很可能是手续费的设置问题
+        BigInteger gasLimit = BigInteger.valueOf(60000L);
+        //ERC20代币合约方法
+        Function function = new Function(
+                "transfer",
+                Arrays.asList(new Address(toAddress), new Uint256(value.toBigIntegerExact())),
+                Collections.singletonList(new TypeReference<Type>() {
+                }));
+        //创建RawTransaction交易对象
+        String encodedFunction = FunctionEncoder.encode(function);
+        RawTransaction rawTransaction = RawTransaction.createTransaction(nonce, gasPrice, gasLimit,
+                contractAddress, encodedFunction);
+
+        //签名Transaction
+        byte[] signMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+        String hexValue = Numeric.toHexString(signMessage);
+        //发送交易
+        EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(hexValue).sendAsync().get();
+        String hash = ethSendTransaction.getTransactionHash();
+        return hash;
+    }
+
+    /**
+     * 获取gas-price
+     *
+     * @param web3j
+     * @return
+     */
+    private static BigInteger getGasPrice(Web3j web3j) {
+        try {
+            EthGasPrice ethGasPrice = web3j.ethGasPrice().sendAsync().get();
+            if (ethGasPrice == null) {
+                log.error("GetGasPriceError");
+                return null;
+            }
+            return ethGasPrice.getGasPrice();
+        } catch (Throwable t) {
+            log.error(t.getMessage(), t);
+        }
+        return null;
+    }
+
+    /**
+     * 获取nonce
+     *
+     * @param web3j
+     * @param address
+     * @return
+     */
+    private static BigInteger getNonce(Web3j web3j, String address) {
+        try {
+            EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(address, DefaultBlockParameterName.PENDING).send();
+            if (ethGetTransactionCount == null) {
+                log.error("GetNonceError:" + address);
+                return null;
+            }
+            return ethGetTransactionCount.getTransactionCount();
+        } catch (Throwable t) {
+            log.error("GetNonceError:" + address);
+        }
+        return null;
+    }
+
+    /**
+     * 查询代币精度
+     *
+     * @param web3j
+     * @param contractAddress
+     * @return
+     */
+    public static int getTokenDecimals(Web3j web3j, String contractAddress) {
+        String methodName = "decimals";
+        String fromAddr = emptyAddress;
+        int decimal = 0;
         List<Type> inputParameters = new ArrayList<>();
         List<TypeReference<?>> outputParameters = new ArrayList<>();
 
-        Address tAddress = new Address(toAddress);
-
-        Uint256 value = new Uint256(amount);
-        inputParameters.add(tAddress);
-        inputParameters.add(value);
-
-        TypeReference<Bool> typeReference = new TypeReference<Bool>() {
+        TypeReference<Uint8> typeReference = new TypeReference<Uint8>() {
         };
         outputParameters.add(typeReference);
 
         Function function = new Function(methodName, inputParameters, outputParameters);
 
         String data = FunctionEncoder.encode(function);
+        Transaction transaction = Transaction.createEthCallTransaction(fromAddr, contractAddress, data);
 
-        String fromAddress = getWalletAddress(walletName, password);
-        EthGetTransactionCount ethGetTransactionCount = web3j
-                .ethGetTransactionCount(fromAddress, DefaultBlockParameterName.PENDING).sendAsync().get();
-        BigInteger nonce = ethGetTransactionCount.getTransactionCount();
-        BigInteger gasPrice = Convert.toWei(BigDecimal.valueOf(5), Convert.Unit.GWEI).toBigInteger();
-
-        Transaction transaction = Transaction.createFunctionCallTransaction(fromAddress, nonce, gasPrice, BigInteger.valueOf(60000), contractAddress, data);
-
-        EthSendTransaction ethSendTransaction = web3j.ethSendTransaction(transaction).sendAsync().get();
-        String txHash = ethSendTransaction.getTransactionHash();
-        return txHash;
+        EthCall ethCall;
+        try {
+            ethCall = web3j.ethCall(transaction, DefaultBlockParameterName.LATEST).sendAsync().get();
+            List<Type> results = FunctionReturnDecoder.decode(ethCall.getValue(), function.getOutputParameters());
+            decimal = Integer.parseInt(results.get(0).getValue().toString());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        return decimal;
     }
 }
 
